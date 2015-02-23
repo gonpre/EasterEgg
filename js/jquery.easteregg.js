742b456e345c2ae616cf162e4d990ab59d465789
Francisco Gonzalez <gonpre@gmail.com>
2015-02-23 11:06:41 -0700

    - Added Konami Code - Changed the behaviour to use an array of char codes instead of the chars, this to allow the use of other keys

--- js/jquery.easteregg.js
+++ js/jquery.easteregg.js
@@ -14,53 +14,77 @@
 
     $.fn.easteregg.default_settings = {
         delay_time : 1000,
-        eggsters   : []
+        eggsters   : [
+            {
+                keys : [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 66, 65],
+                callback : null
+            }
+        ]
+    };
+
+    function is_same_array(array1, array2) {
+        return array1.lenght == array2.lenght && array1.every(function(element, index) {
+            return element === array2[index];
+        });
+    }
+
+    var EasterEgg = function(settings){
+        this._settings     = settings;
+        this._pressed_keys = [];
+        this._timer;
+    };
+
+    EasterEgg.VERSION = '1.0.0';
+
+    EasterEgg.prototype = {
+        init : function() {
+            var _self = this;
+
+            $(document).on('keydown', function(event){
+                clearInterval(_self._timer);
+
+                _self._timer        = _self.getTimer();
+                _self._pressed_keys.push(event.keyCode);
+
+                _self._settings.eggsters.forEach(function(eggster){
+                    if (is_same_array(eggster.keys, _self._pressed_keys)) {
+                        _self.clearKeys();
+                        if ($.type(eggster.callback) === 'function') {
+                            eggster.callback();
+                        }
+                    }
+                });
+            });
+
+            _self._timer = _self.getTimer();
+        },
+        setKonami : function(callback) {
+            this._settings.eggsters[0].callback = callback;
+        },
+        addEgg : function(keys, callback) {
+            if (!($.type(keys) === 'array' || $.type(keys) === 'string') || keys.lenght == 0 || $.type(callback) !== 'function') {
+                return false;
+            }
+
+            if ($.type(keys) === 'string') {
+                var keys = keys.split('');
+
+                keys.forEach(function(element, index, array){
+                    array[index] = element.toUpperCase().charCodeAt();
+                });
+            }
+
+            this._settings.eggsters.push({keys:keys, callback:callback});
+        },
+        clearKeys : function() {
+            this._pressed_keys = [];
+        },
+        getTimer : function() {
+            var _self = this;
+
+            return setInterval(function(){
+                _self.clearKeys();
+            }, _self._settings.delay_time);
+        },
     };
 })(jQuery);
-
-var EasterEgg = function(settings){
-    this._settings     = settings;
-    this._pressed_keys = '';
-    this._timer;
-};
-
-EasterEgg.VERSION = '1.0.0';
-
-EasterEgg.prototype = {
-    addEgg : function(keys, callback) {
-        if ($.type(keys) !== 'string' || keys.lenght == 0 || $.type(callback) !== 'function') {
-            return false;
-        }
-
-        this._settings.eggsters.push({keys:keys, callback:callback});
-    },
-    init : function() {
-        var _self = this;
-
-        $(document).on('keypress', function(event){
-            clearInterval(_self._timer);
-
-            _self._timer        = _self.getTimer();
-            _self._pressed_keys += String.fromCharCode(event.which);
-
-            _self._settings.eggsters.forEach(function(eggster){
-                if (eggster.keys == _self._pressed_keys) {
-                    _self.clearKeys();
-                    eggster.callback();
-                }
-            });
-        });
-
-        _self._timer = _self.getTimer();
-    },
-    clearKeys : function() {
-        this._pressed_keys = '';
-    },
-    getTimer : function() {
-        var _self = this;
-
-        return setInterval(function(){
-            _self.clearKeys();
-        }, _self._settings.delay_time);
-    },
-};
